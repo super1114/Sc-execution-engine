bcf393dfbe5181fb031f40b6f0f0d98764ce5ffb
super1114 <super1114dev@gmail.com>
2023-08-28 20:27:50 +0200

    feat: add cli commands for singular vesting

--- app/actions.ts
+++ app/actions.ts
@@ -2,8 +2,21 @@
  * Need to build transaction instruction from the params of cli commands
  */
 
-import { LAMPORTS_PER_SOL, PublicKey, SystemProgram } from "@solana/web3.js";
-import { executeTransaction, provider } from "./lib";
+import {
+  Keypair,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+  SystemProgram,
+} from "@solana/web3.js";
+
+import {
+  claim,
+  createPool,
+  deposit,
+  executeTransaction,
+  nominateReceiver,
+  provider,
+} from "./lib";
 
 /// Transfer Native SOL from provider wallet to destination
 export const transferSOL = async (toPubkey: PublicKey, amount: number) => {
@@ -21,5 +34,101 @@ export const transferSOL = async (toPubkey: PublicKey, amount: number) => {
     .then((txId) => console.log("Executed TxId=", txId))
     .catch((e) =>
       console.error("Failed execution:" + e.message || JSON.stringify(e))
+    );
+};
+
+export const create = async (
+  mint: PublicKey,
+  amount: number,
+  lockingPeriod: number,
+  minSign: number,
+  remainSigners: PublicKey[]
+) => {
+  const baseKey: Keypair = Keypair.generate();
+  const tx = await createPool(
+    baseKey,
+    mint,
+    BigInt(amount),
+    lockingPeriod,
+    minSign,
+    remainSigners
+  );
+  tx.feePayer = provider.publicKey;
+  tx.recentBlockhash = (
+    await provider.connection.getLatestBlockhash()
+  ).blockhash;
+
+  await provider
+    .sendAndConfirm(tx, [baseKey], {
+      commitment: "finalized",
+    })
+    .then((txId) => console.log("Created TxId=", txId))
+    .catch((e) =>
+      console.error("Failed creation:" + e.message || JSON.stringify(e))
+    );
+};
+
+export const depositFund = async (
+  baseKey: PublicKey,
+  remainSigners: Keypair[]
+) => {
+  const tx = await deposit(
+    baseKey,
+    remainSigners.map((keypair) => keypair.publicKey)
+  );
+  tx.feePayer = provider.publicKey;
+  tx.recentBlockhash = (
+    await provider.connection.getLatestBlockhash()
+  ).blockhash;
+
+  await provider
+    .sendAndConfirm(tx, remainSigners, {
+      commitment: "finalized",
+    })
+    .then((txId) => console.log("Deposited TxId=", txId))
+    .catch((e) =>
+      console.error("Failed deposition:" + e.message || JSON.stringify(e))
+    );
+};
+
+export const setRecipient = async (
+  baseKey: PublicKey,
+  receiver: PublicKey,
+  remainSigners: Keypair[]
+) => {
+  const tx = await nominateReceiver(
+    baseKey,
+    receiver,
+    remainSigners.map((keypair) => keypair.publicKey)
+  );
+  tx.feePayer = provider.publicKey;
+  tx.recentBlockhash = (
+    await provider.connection.getLatestBlockhash()
+  ).blockhash;
+
+  await provider
+    .sendAndConfirm(tx, remainSigners, {
+      commitment: "finalized",
+    })
+    .then((txId) => console.log("Receiver configured TxId=", txId))
+    .catch((e) =>
+      console.error("Failed configuration:" + e.message || JSON.stringify(e))
+    );
+};
+
+export const claimTokens = async (baseKey: PublicKey) => {
+  const tx = await claim(baseKey);
+  tx.feePayer = provider.publicKey;
+  tx.recentBlockhash = (
+    await provider.connection.getLatestBlockhash()
+  ).blockhash;
+
+  await provider
+    .sendAndConfirm(tx, [], {
+      commitment: "finalized",
+    })
+    .then((txId) => console.log("Claimed TxId=", txId))
+    .catch((e) =>
+      console.error("Failed claim:" + e.message || JSON.stringify(e))
     );
 };
--- app/cli.ts
+++ app/cli.ts
@@ -2,7 +2,13 @@
 import { program } from "commander";
 import { PublicKey } from "@solana/web3.js";
 import { setNetwork } from "./lib";
-import { transferSOL } from "./actions";
+import {
+  claimTokens,
+  create,
+  depositFund,
+  setRecipient,
+  transferSOL,
+} from "./actions";
 
 program.version("0.0.1");
 
@@ -31,6 +37,105 @@ programCommand("sol_transfer")
     await setNetwork(env);
 
     await transferSOL(new PublicKey(target), parseFloat(amount));
+  });
+
+programCommand("create")
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  .description(
+    `
+    Create vesting pool as sender
+  ${clusterHelp}`
+  )
+  .requiredOption("-t, --token <string>", "The token mint address")
+  .requiredOption("-a, --amount <number>", "Transfer token amount")
+  .requiredOption(
+    "-l, --locking_period <number>",
+    "Transfer locking time in seconds"
+  )
+  // TODO: need to specify keypairs for multi sign
+  // min_sign should be 1 for singlar Transfer
+  .requiredOption("-s, --min_sign <number>", "Minimal signatures for multi sig")
+  .action(async (directory, cmd) => {
+    const { env, token, amount, lockingPeriod, minSign } = cmd.opts();
+
+    if (isNaN(parseFloat(amount)) || parseFloat(amount) <= 0) {
+      console.log("Error input amount");
+      return;
+    }
+    if (isNaN(parseInt(lockingPeriod)) || parseInt(lockingPeriod) <= 0) {
+      console.log("Error input amount");
+      return;
+    }
+    if (isNaN(parseInt(minSign)) || parseInt(minSign) <= 0) {
+      console.log("Error input amount");
+      return;
+    }
+    console.log("Solana config: ", env);
+    await setNetwork(env);
+
+    // TODO: need to change minSign & remainSigners for multi sign Transfer
+    await create(
+      new PublicKey(token),
+      parseInt(amount),
+      parseInt(lockingPeriod),
+      1,
+      []
+    );
+  });
+
+programCommand("deposit")
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  .description(
+    `
+    Deposit vesting token to pool as sender
+  ${clusterHelp}`
+  )
+  .requiredOption("-b, --base <string>", "The base key of vesting pool")
+  // TODO: need to specify keypairs for multi sign
+  .action(async (directory, cmd) => {
+    const { env, base } = cmd.opts();
+
+    console.log("Solana config: ", env);
+    await setNetwork(env);
+
+    // TODO: need to change remainSigners for multi sign Transfer
+    await depositFund(new PublicKey(base), []);
+  });
+
+programCommand("nominate")
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  .description(
+    `
+    Set recipient of vesting as sender
+  ${clusterHelp}`
+  )
+  .requiredOption("-b, --base <string>", "The base key of vesting pool")
+  .requiredOption("-r, --receiver <string>", "The recipient address")
+  .action(async (directory, cmd) => {
+    const { env, base, receiver } = cmd.opts();
+
+    console.log("Solana config: ", env);
+    await setNetwork(env);
+
+    // TODO: need to change remainSigners for multi sign Transfer
+    await setRecipient(new PublicKey(base), new PublicKey(receiver), []);
+  });
+
+programCommand("claim")
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  .description(
+    `
+    Claim vesting tokens as receiver
+  ${clusterHelp}`
+  )
+  .requiredOption("-b, --base <string>", "The base key of vesting pool")
+  .action(async (directory, cmd) => {
+    const { env, base } = cmd.opts();
+
+    console.log("Solana config: ", env);
+    await setNetwork(env);
+
+    await claimTokens(new PublicKey(base));
   });
 
 function programCommand(name: string) {
